8587e478ad05f909b691bdab902a88c8bed772c2
kol-di <60890747+kol-di@users.noreply.github.com>
2022-11-10 15:46:13 +0500

    refactoring and stdout execution status

--- PinterestScraper/pinterest_scraper.py
+++ PinterestScraper/pinterest_scraper.py
@@ -19,14 +19,15 @@ class PageEndError(Exception):
 
 
 def get_image_batch(driver):
-    scrollnum = 1
-    sleepTimer = 2
+    sleep_timer = 1
 
+    batch_cnt = 0
     while True:
+        batch_cnt += 1
+        print(f'Batch {batch_cnt}')
         last_height = driver.execute_script("return document.body.scrollHeight")
         driver.execute_script("window.scrollBy(0,5000)")
-        print('scroll-down')
-        time.sleep(sleepTimer)
+        time.sleep(sleep_timer)
         new_height = driver.execute_script("return document.body.scrollHeight")
 
         if last_height == new_height:
@@ -36,6 +37,22 @@ def get_image_batch(driver):
         yield
 
 
+def save_images(image_tags):
+    for tag in image_tags:
+        img_link = tag.get('src')
+        img_name = img_link.rsplit('/', 1)[-1].strip('.jpg') + '.png'
+        with open(img_name, 'wb') as f:
+            img = requests.get(img_link)
+            f.write(img.content)
+
+
+def cd_to_image_folder(storage_folder):
+    storage_path = os.path.join(os.getcwd(), storage_folder)
+    if not os.path.exists(storage_path):
+        os.mkdir(storage_path)
+    os.chdir(storage_path)
+
+
 def get_image(tag, storage_folder, img_cnt):
     url = f'https://in.pinterest.com/search/pins/?q={tag}'
     options = webdriver.ChromeOptions()
@@ -43,34 +60,32 @@ def get_image(tag, storage_folder, img_cnt):
     driver = webdriver.Chrome(executable_path='chromedriver.exe', options=options)
     driver.get(url)
 
+    cd_to_image_folder(storage_folder)
+
     soup = BeautifulSoup(driver.page_source, 'html5lib')
-    prev_soup = set(soup.find_all('img'))
-
+    prev_window_soup = set(soup.find_all('img'))
+    save_images(prev_window_soup)
+    cur_img_cnt = len(prev_window_soup)
+    print(f'Collected {cur_img_cnt} new images')
+    image_batch_collector = get_image_batch(driver)
     try:
-        cur_img_cnt = 0
         while cur_img_cnt < img_cnt:
-            next(get_image_batch(driver))
+            next(image_batch_collector)
             soup = BeautifulSoup(driver.page_source, 'html5lib')
-            cur_img_cnt = len(soup.find_all('img'))
-            print('Images in soup', cur_img_cnt)
-            new_soup = set(soup.find_all('img'))
-            print('New images', len(prev_soup.intersection(new_soup)))
-            prev_soup = new_soup
+
+            next_window_soup = set(soup.find_all('img'))
+            unique_soup = next_window_soup - prev_window_soup
+            save_images(unique_soup)
+
+            print(f'Collected {len(unique_soup)} new images, '
+                  f'{len(next_window_soup.intersection(prev_window_soup))} old images.')
+            prev_window_soup = next_window_soup
+            cur_img_cnt += len(unique_soup)
     except PageEndError:
+        print(f'Page exhausted, collected {cur_img_cnt} images')
         pass
-
-    storage_path = os.path.join(os.getcwd(), storage_folder)
-    if not os.path.exists(storage_path):
-        os.mkdir(storage_path)
-    os.chdir(storage_path)
-
-    for link in soup.find_all('img'):
-        img_link = link.get('src')
-        img_name = img_link.rsplit('/', 1)[-1].strip('.jpg') + '.png'
-        print(img_name)
-        with open(img_name, 'wb') as f:
-            img = requests.get(img_link)
-            f.write(img.content)
+    else:
+        print(f'Collected {cur_img_cnt} images')
 
 
 get_image('breakcore', 'breakcore', 100)
